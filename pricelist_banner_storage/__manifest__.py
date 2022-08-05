a1e8df2e5128c38366771b9d260e3ce424e77fb3
Odoo Aktiv <odoo@aktivsoftware.com>
2022-08-05 11:42:43 +0530

    [INIT] initial commit

---
+++ pricelist_banner_storage/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name": "Banners on pricelist (Discount) Storage",
+    "author": "Aktiv Software",
+    "website": "https://aktivsoftware.com",
+    "category": "Website",
+    "summary": """ Store Banners
+""",
+    "version": "14.0.1.0.0",
+    "images": [],
+    "auto_install": False,
+    "application": False,
+    "installable": True,
+}
